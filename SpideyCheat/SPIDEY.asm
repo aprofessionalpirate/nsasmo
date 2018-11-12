615c38592162d38ae350a6707b7e8a999ef89ca1
Jason <jaz_b11@hotmail.com>
2018-11-12 22:38:49 +1100

    Add SpideyCheat source

---
+++ SpideyCheat/SPIDEY.asm
@@ -0,0 +1,300 @@
+;;; Segment code (0C00:0100)
+
+;; fn0C00_0100: 0C00:0100
+fn0C00_0100 proc
+	jmp	055A
+0C00:0103          5F 7C 85 88 0E 07 0D 53 70 69 64 65 72    _|.....Spider
+0C00:0110 6D 61 6E 20 63 68 65 61 74 2C 20 62 79 20 4D 69 man cheat, by Mi
+0C00:0120 63 68 61 65 6C 20 59 61 6B 75 62 6F 76 69 63 68 chael Yakubovich
+0C00:0130 2C 20 44 65 63 65 6D 62 65 72 20 31 39 39 32 2E , December 1992.
+0C00:0140 0D 0A 0D 0A 55 73 65 20 2B 2F 2D 20 6F 6E 20 74 ....Use +/- on t
+0C00:0150 68 65 20 6B 65 79 70 61 64 20 74 6F 20 6D 6F 76 he keypad to mov
+0C00:0160 65 20 74 6F 20 64 69 66 66 65 72 65 6E 74 20 6C e to different l
+0C00:0170 65 76 65 6C 73 2C 20 75 73 65 20 45 4E 54 45 52 evels, use ENTER
+0C00:0180 20 74 6F 20 75 70 64 61 74 65 20 0D 0A 74 68 65  to update ..the
+0C00:0190 20 69 6E 74 65 72 6E 61 6C 20 63 6F 75 6E 74 65  internal counte
+0C00:01A0 72 20 6F 66 20 6C 65 76 65 6C 73 20 69 66 20 74 r of levels if t
+0C00:01B0 68 65 20 73 63 72 65 65 6E 20 64 69 64 6E 27 74 he screen didn't
+0C00:01C0 20 63 68 61 6E 67 65 2E 0D 0A 41 6C 73 6F 20 6E  change...Also n
+0C00:01D0 6F 74 65 3A 20 75 6E 6C 65 73 73 20 79 6F 75 20 ote: unless you 
+0C00:01E0 75 73 65 64 20 2B 2F 2D 20 6F 72 20 45 4E 54 45 used +/- or ENTE
+0C00:01F0 52 20 74 68 65 20 69 6E 74 65 72 6E 61 6C 20 63 R the internal c
+0C00:0200 6F 75 6E 74 65 72 20 69 73 20 6E 6F 74 20 75 70 ounter is not up
+0C00:0210 64 61 74 65 64 2C 0D 0A 61 6C 74 68 6F 75 67 68 dated,..although
+0C00:0220 20 74 68 65 20 6E 75 6D 62 65 72 20 64 69 73 70  the number disp
+0C00:0230 6C 61 79 65 64 20 69 6E 20 74 68 65 20 63 6F 72 layed in the cor
+0C00:0240 6E 65 72 20 69 73 20 63 68 61 6E 67 65 64 2E 0D ner is changed..
+0C00:0250 0A 53 6F 20 69 66 20 79 6F 75 20 6D 6F 76 65 64 .So if you moved
+0C00:0260 20 74 6F 20 61 6E 6F 74 68 65 72 20 6C 65 76 65  to another leve
+0C00:0270 6C 20 62 79 20 6A 75 73 74 20 70 6C 61 79 69 6E l by just playin
+0C00:0280 67 20 74 68 65 20 67 61 6D 65 2C 0D 0A 70 72 65 g the game,..pre
+0C00:0290 73 73 20 45 4E 54 45 52 20 62 65 66 6F 72 65 20 ss ENTER before 
+0C00:02A0 75 73 69 6E 67 20 2B 2F 2D 20 61 67 61 69 6E 2E using +/- again.
+0C00:02B0 0D 0A 5D 2F 5B 20 6B 65 79 73 20 64 6F 20 74 68 ..]/[ keys do th
+0C00:02C0 65 20 73 61 6D 65 20 61 73 20 2B 2F 2D 20 6F 6E e same as +/- on
+0C00:02D0 20 74 68 65 20 6B 65 79 70 61 64 2E 0D 0A 55 73  the keypad...Us
+0C00:02E0 65 20 27 35 27 20 6F 6E 20 74 68 65 20 6B 65 79 e '5' on the key
+0C00:02F0 70 61 64 20 28 70 72 65 73 73 20 61 6E 64 20 68 pad (press and h
+0C00:0300 6F 6C 64 20 64 6F 77 6E 29 20 74 6F 20 70 61 75 old down) to pau
+0C00:0310 73 65 20 74 68 65 20 67 61 6D 65 2E 0D 0A 54 68 se the game...Th
+0C00:0320 69 73 20 70 72 6F 67 72 61 6D 20 61 6C 73 6F 20 is program also 
+0C00:0330 6D 61 6B 65 73 20 74 68 65 20 53 70 69 64 65 72 makes the Spider
+0C00:0340 6D 61 6E 20 69 6E 76 75 6C 6E 65 72 61 62 6C 65 man invulnerable
+0C00:0350 20 61 6E 64 20 62 79 70 61 73 73 65 73 20 74 68  and bypasses th
+0C00:0360 65 20 64 6F 63 2D 63 68 65 63 6B 2E 1A 0A 0D 0A e doc-check.....
+0C00:0370 50 72 65 73 73 20 45 53 43 20 74 6F 20 71 75 69 Press ESC to qui
+0C00:0380 74 2C 20 61 6E 79 20 6F 74 68 65 72 20 6B 65 79 t, any other key
+0C00:0390 20 63 6F 6E 74 69 6E 75 65 73 2E 2E 0D 0A 24 CF  continues....$.
+0C00:03A0 9C 80 FC 0F 74 03 E9 AE 00 50 1E 55 1E 58 2E A3 ....t....P.U.X..
+0C00:03B0 23 05 C6 06 42 97 01 8B EC 8B 46 0A 8E D8 81 3E #...B.....F....>
+0C00:03C0 48 33 A3 0C 75 0B C7 06 48 33 90 90 C6 06 4A 33 H3..u...H3....J3
+0C00:03D0 90 81 3E 40 11 A0 D8 75 4E C7 06 40 11 A0 7F C7 ..>@...uN..@....
+0C00:03E0 06 42 11 00 8A C7 06 44 11 26 0C C7 06 46 11 1D .B.....D.&...F..
+0C00:03F0 3A C7 06 48 11 E0 74 C7 06 4A 11 08 A2 C7 06 4C :..H..t..J.....L
+0C00:0400 11 0C 1D C7 06 4E 11 C6 06 C7 06 50 11 0E 1D C7 .....N.....P....
+0C00:0410 06 52 11 FF C7 C7 06 54 11 06 D8 C7 06 56 11 92 .R.....T.....V..
+0C00:0420 C0 C7 06 58 11 C0 C3 81 3E 21 01 E8 C1 75 25 80 ...X....>!...u%.
+0C00:0430 3E 23 01 37 75 1E C7 06 24 01 90 90 C7 06 26 01 >#.7u...$.....&.
+0C00:0440 90 90 C7 06 28 01 90 90 C7 06 2A 01 90 90 C7 06 ....(.....*.....
+0C00:0450 2C 01 90 90 5D 1F 58 9D EA 00 00 00 00 9C 50 1E ,...].X.......P.
+0C00:0460 32 C0 E4 60 3C 4C 74 FA 3C 4E 74 07 3C 1B 74 03 2..`<Lt.<Nt.<.t.
+0C00:0470 EB 27 90 2E A1 23 05 8E D8 2E A0 25 05 FE C0 3C .'...#.....%...<
+0C00:0480 3F 76 02 B0 01 2E A2 25 05 A2 7F 00 32 E4 A3 0C ?v.....%....2...
+0C00:0490 1D C6 06 0E 1D FF E9 81 00 3C 4A 74 07 3C 1A 74 .........<Jt.<.t
+0C00:04A0 03 EB 27 90 2E A1 23 05 8E D8 2E A0 25 05 FE C8 ..'...#.....%...
+0C00:04B0 3C 00 75 02 B0 3F 2E A2 25 05 A2 7F 00 32 E4 A3 <.u..?..%....2..
+0C00:04C0 0C 1D C6 06 0E 1D FF EB 51 90 3C CE 74 07 3C 9B ........Q.<.t.<.
+0C00:04D0 74 03 EB 0F 90 2E A1 23 05 8E D8 C6 06 0E 1D FF t......#........
+0C00:04E0 EB 38 90 3C CA 74 07 3C 9A 74 03 EB 0F 90 2E A1 .8.<.t.<.t......
+0C00:04F0 23 05 8E D8 C6 06 0E 1D FF EB 1F 90 3C 1C 75 1A #...........<.u.
+0C00:0500 2E A1 23 05 8E D8 A0 7F 00 2E A2 25 05 32 E4 A3 ..#........%.2..
+0C00:0510 0C 1D C6 06 0E 1D FF EB 01 90 1F 58 9D EA 00 00 ...........X....
+0C00:0520 00 00 00 00 00 01 E0 8C F3 34 84 86 85 8D 86 F7 .........4......
+0C00:0530 8B 34 8F 81 34 F0 F7 8B F7 8D F3 F0 37 CA CA 14 .4..4.......7...
+
+l0C00_0540:
+	mov	di,0526
+
+l0C00_0543:
+	mov	dl,[di]
+	inc	di
+	xor	dl,4D
+	sub	dl,59
+	mov	ah,02
+	int	21
+	cmp	dl,00
+	jnz	0543
+
+l0C00_0555:
+	mov	ax,4CFF
+	int	21
+
+l0C00_055A:
+	mov	di,0109
+	mov	cx,[0107]
+	xor	ax,ax
+	xor	bx,bx
+
+l0C00_0565:
+	add	ax,[di]
+	xor	bx,[di]
+	inc	di
+	loop	0565
+
+l0C00_056C:
+	cmp	ax,[0103]
+	jnz	0540
+
+l0C00_0572:
+	cmp	bx,[0105]
+	jnz	0540
+
+l0C00_0578:
+	mov	sp,0817
+	push	cs
+	pop	ds
+	mov	byte ptr [036C],0D
+	mov	ah,09
+	mov	dx,010A
+	int	21
+	mov	ax,0800
+	int	21
+	cmp	al,03
+	jnz	0595
+
+l0C00_0592:
+	jmp	06A7
+
+l0C00_0595:
+	cmp	al,1B
+	jnz	059C
+
+l0C00_0599:
+	jmp	06A7
+
+l0C00_059C:
+	push	cs
+	pop	es
+	mov	bx,0081
+	nop
+
+l0C00_05A2:
+	mov	ah,4A
+	inc	bx
+	int	21
+	jc	05A2
+
+l0C00_05A9:
+	mov	ah,19
+	int	21
+	add	[0714],al
+	mov	ah,47
+	mov	dl,00
+	mov	si,0717
+	int	21
+	mov	di,0717
+	cmp	byte ptr [di],00
+	jz	05CD
+
+l0C00_05C2:
+	xor	ax,ax
+	mov	cx,0100
+
+l0C00_05C7:
+	repne scasb
+
+l0C00_05C9:
+	mov	byte ptr [di-01],5C
+
+l0C00_05CD:
+	mov	si,06C1
+	xor	cx,cx
+
+l0C00_05D2:
+	inc	cx
+	inc	si
+	cmp	byte ptr [si],00
+	jnz	05D2
+
+l0C00_05D9:
+	mov	si,06C2
+
+l0C00_05DC:
+	rep movsb
+
+l0C00_05DE:
+	push	cs
+	pop	ax
+	mov	[06D2],ax
+	mov	ax,06D4
+	mov	[06D0],ax
+	mov	ax,[002C]
+	mov	[06CE],ax
+	push	cs
+	pop	es
+	push	cs
+	pop	ds
+	mov	si,0080
+	mov	di,06D4
+	xor	cx,cx
+	mov	cl,[si]
+	inc	cx
+	inc	cx
+	cld
+
+l0C00_0600:
+	rep movsb
+
+l0C00_0602:
+	push	cs
+	pop	ds
+	mov	ax,351B
+	int	21
+	mov	cs:[06AE],es
+	mov	cs:[06AC],bx
+	push	cs
+	pop	ds
+	mov	ax,3510
+	int	21
+	mov	cs:[045B],es
+	mov	cs:[0459],bx
+	push	cs
+	pop	ds
+	mov	ax,3509
+	int	21
+	mov	cs:[0520],es
+	mov	cs:[051E],bx
+	mov	ax,2510
+	push	cs
+	pop	ds
+	mov	dx,03A0
+	int	21
+	mov	ax,2509
+	push	cs
+	pop	ds
+	mov	dx,045D
+	int	21
+	mov	ax,251B
+	push	cs
+	pop	ds
+	mov	dx,039F
+	int	21
+	mov	ax,2523
+	push	cs
+	pop	ds
+	mov	dx,039F
+	int	21
+	push	cs
+	pop	ds
+	push	cs
+	pop	es
+	mov	bx,06CE
+	mov	dx,0714
+	mov	ax,4B00
+	int	21
+	jnc	0675
+
+l0C00_066E:
+	mov	ah,09
+	mov	dx,06B0
+	int	21
+
+l0C00_0675:
+	mov	dx,cs:[0459]
+	mov	ds,cs:[045B]
+	mov	ax,2510
+	int	21
+	mov	dx,cs:[051E]
+	mov	ds,cs:[0520]
+	mov	ax,2509
+	int	21
+	mov	dx,cs:[06AC]
+	mov	ds,cs:[06AE]
+	mov	ax,251B
+	int	21
+	mov	ax,0CFF
+	int	21
+
+l0C00_06A7:
+	mov	ax,4C00
+	int	21
+0C00:06AC                                     00 00 00 00             ....
+0C00:06B0 45 52 52 4F 52 3A 20 63 61 6E 27 74 20 6C 6F 61 ERROR: can't loa
+0C00:06C0 64 20 73 70 69 64 65 79 2E 65 78 65 00 24 00 00 d spidey.exe.$..
+0C00:06D0 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ................
+; ...
+0C00:0710 00 00 00 00 41 3A 5C 00 00 00 00 00 00 00 00 00 ....A:\.........
+0C00:0720 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 ................
+; ...
+0C00:0760 00 00 00 00 00 00 00 0D 0A 09 4D 59 20 53 54 41 ..........MY STA
+0C00:0770 43 4B 0D 0A 09 4D 59 20 53 54 41 43 4B 0D 0A 09 CK...MY STACK...
+0C00:0780 4D 59 20 53 54 41 43 4B 0D 0A 09 4D 59 20 53 54 MY STACK...MY ST
+0C00:0790 41 43 4B 0D 0A 09 4D 59 20 53 54 41 43 4B 0D 0A ACK...MY STACK..
+0C00:07A0 09 4D 59 20 53 54 41 43 4B 0D 0A 09 4D 59 20 53 .MY STACK...MY S
+0C00:07B0 54 41 43 4B 0D 0A 09 4D 59 20 53 54 41 43 4B 0D TACK...MY STACK.
+0C00:07C0 0A 09 4D 59 20 53 54 41 43 4B 0D 0A 09 4D 59 20 ..MY STACK...MY 
+0C00:07D0 53 54 41 43 4B 0D 0A 09 4D 59 20 53 54 41 43 4B STACK...MY STACK
+0C00:07E0 0D 0A 09 4D 59 20 53 54 41 43 4B 0D 0A 09 4D 59 ...MY STACK...MY
+0C00:07F0 20 53 54 41 43 4B 0D 0A 09 4D 59 20 53 54 41 43  STACK...MY STAC
+0C00:0800 4B 0D 0A 09 4D 59 20 53 54 41 43 4B 0D 0A 09 4D K...MY STACK...M
+0C00:0810 59 20 53 54 41 43 4B 00                         Y STACK.       
